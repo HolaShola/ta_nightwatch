fb8eb0667ae6eb971571ed52628a8b8a7961a40c
Yauheni_Sholakhau <Yauheni_Sholakhau@epam.com>
2017-10-23 11:19:24 +0300

    add Yandex mail tests

--- tests/yandex_mail.js
+++ tests/yandex_mail.js
@@ -1,26 +1,26 @@
 module.exports = {
-//  'Yandex mail fail enter' : function (browser) {
-//    browser
-//      .url('https://mail.yandex.by')
-//      .waitForElementVisible('body', 1000)
-//      .setValue('input[name=login]', 'nightwatch')
-//      .setValue('input[type=password]', 'pass123')
-//      .click('button[type=submit]')
-//      .pause(1000)
-//      .assert.elementPresent('.passport-Domik-Form-Error_active')
-//      .end();
-//  },
+  'Yandex mail fail enter' : function (browser) {
+    browser
+      .url('https://mail.yandex.by')
+      .waitForElementVisible('body', 1000)
+      .setValue('input[name=login]', 'nightwatch')
+      .setValue('input[type=password]', 'pass123')
+      .click('button[type=submit]')
+      .pause(1000)
+      .assert.elementPresent('.passport-Domik-Form-Error_active')
+      .end();
+  },
 
   'Yandex mail success enter' : function (browser) {
-      browser
-        .url('https://mail.yandex.by')
-        .waitForElementVisible('body', 1000)
-        .setValue('input[name=login]', 'ola-shola')
-        .setValue('input[type=password]', 'xhola_sholax')
-        .pause(2000)
-        .click('button[type=submit]')
-        .pause(4000)
-        .assert.elementPresent('.mail-User-Name')
-        .end();
-    }
+    browser
+      .url('https://mail.yandex.by')
+      .waitForElementVisible('body', 1000)
+      .setValue('input[name=login]', 'hola-shola')
+      .setValue('input[type=password]', 'xhola_sholax')
+      .pause(2000)
+      .click('button[type=submit]')
+      .pause(4000)
+      .assert.elementPresent('.mail-User-Name')
+      .end();
+  }
 };
