7d64be2eadd862bdc523acec76eb224c5b835ad7
HolaShola <xxjxxx@mail.ru>
2017-10-24 11:48:04 +0300

    add success enter to mail.ru

--- tests/demo.js
+++ tests/demo.js
@@ -1,4 +1,4 @@
-//module.exports = {
+// module.exports = {
 //  'Demo test Google' : function (browser) {
 //    browser
 //      .url('http://www.google.com')
@@ -10,4 +10,4 @@
 //      .assert.containsText('#main', 'Night Watch')
 //      .end();
 //  }
-//};
+// };
--- tests/yandex_mail.js
+++ tests/yandex_mail.js
@@ -1,26 +1,61 @@
 module.exports = {
-//  'Yandex mail fail enter' : function (browser) {
-//    browser
-//      .url('https://mail.yandex.by')
-//      .waitForElementVisible('body', 1000)
-//      .setValue('input[name=login]', 'nightwatch')
-//      .setValue('input[type=password]', 'pass123')
-//      .click('button[type=submit]')
-//      .pause(1000)
-//      .assert.elementPresent('.passport-Domik-Form-Error_active')
-//      .end();
-//  },
+//    'mail.ru fail enter' : function (browser) {
+//        browser
+//            .url('https://mail.ru/')
+//            .waitForElementVisible('body', 1000)
+//            .setValue('input[name="login"]', 'ola-shola')
+//            .setValue('input[name="password"]', 'x123holasholaxx')
+//            .click('input[type=submit]')
+//            .pause(8000)
+//            .assert.containsText("div[id='mailbox:error']", "Неверное имя или пароль")
+//            .assert.containsText("div.b-login__errors", "Замечены подозрительные попытки входа в ваш почтовый ящик.")
+//            .waitForElementVisible("div[id='view']", 3000)
+//            .end();
+//    },
 
-  'Yandex mail success enter' : function (browser) {
-      browser
-        .url('https://mail.yandex.by')
-        .waitForElementVisible('body', 1000)
-        .setValue('input[name=login]', 'ola-shola')
-        .setValue('input[type=password]', 'xhola_sholax')
-        .pause(2000)
-        .click('button[type=submit]')
-        .pause(4000)
-        .assert.elementPresent('.mail-User-Name')
-        .end();
-    }
+//  'Yandex mail success enter' : function (browser) {
+//      browser
+//        .url('https://mail.yandex.by')
+//        .waitForElementVisible('body', 1000)
+//        .setValue('input[name=login]', 'hola-shola')
+//        .setValue('input[type=password]', 'xhola_sholax')
+//        .pause(2000)
+//        .submitForm('form.new-auth-form')
+//        .click('button[type=submit]')
+//        .pause(12000)
+//        .assert.elementPresent('.mail-User-Name')
+//        .click('a[data-provider="gg"]')
+//        .pause(7000)
+//        .end();
+//    }
+
+     'mail.ru success enter' : function (browser) {
+       browser
+         .url('https://mail.ru/')
+         .waitForElementVisible('body', 1000)
+         .setValue('input[name="login"]', 'hola-shola')
+         .setValue('input[name="password"]', 'x123holasholaxx')
+         .click('input[type=submit]')
+         .pause(8000)
+         .assert.containsText("span #PH_user-email", "hola-shola@mail.ru")
+         .end();
+     },
+
+     'mail.ru success enter' : function (browser) {
+        browser
+          .url('https://mail.ru/')
+          .waitForElementVisible('body', 1000)
+          .setValue('input[name="login"]', 'hola-shola')
+          .setValue('input[name="password"]', 'x123holasholaxx')
+          .click('input[type=submit]')
+          .pause(3000)
+          .assert.containsText("span #PH_user-email", "hola-shola@mail.ru")
+          .click(".b-toolbar__item>a")
+          .setValue('textarea[tabindex="4"]', 'hola-shola@mail.ru')
+          .setValue('body #tinymce', '123')
+          .click('div[data-title="Отправить (Ctrl+Enter)"]')
+          .pause(3000)
+          .end();
+      },
+
 };
